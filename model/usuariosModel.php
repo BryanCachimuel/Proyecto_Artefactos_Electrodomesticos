dd3788337af57e92636f0a5758a159cc39e3f0fd
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-05 22:04:25 -0500

    creando la funcionalidad para crear un usuario

---
+++ model/usuariosModel.php
@@ -0,0 +1,19 @@
+<?php
+    class UsuariosModel{
+        private $PDO;
+
+        public function __construct(){
+            require_once("../config/db.php");
+            $pdo = new db();
+            $this->PDO = $pdo->conexion();
+        }
+
+        public function agregarNuevoUsuario($nombre, $correo, $password){
+            $statement = $this->PDO->prepare("INSERT INTO usarios values(null,:nombre,:correo,:password)");
+            $statement->bindParam(":nombre",$nombre);
+            $stament->bindParam(":correo",$correo);
+            $stament->bindParam(":password",$password);
+            return ($stament->execute()) ? true : false;
+        }
+    }
+?>
