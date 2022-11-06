8ee96f070b5af555ad036d4c0022e5d0300a6532
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-05 22:26:27 -0500

    agregando las funcionalidades para poder guardar un usuario desde controlador

---
+++ controller/usuariosController.php
@@ -0,0 +1,39 @@
+<?php 
+    class UsuariosController{
+        private $model;
+        public function __construct(){
+            require_once("../model/usuariosModel.php");
+            $this->model = new UsuariosModel();
+        }
+
+        public function guardarUsuario($nombre, $correo, $password){
+            $valor = $this->model->agregarNuevoUsuario($this->limpiarNombre($nombre),$this->limpiarCorreo($correo),$this->encriptarContrasenia($this->limpiarPassword($password)));
+            return $valor;
+        }
+
+        public function limpiarNombre($campo){
+            $campo = strip_tags($campo);
+            $campo = filter_var($campo, FILTER_UNSAFE_RAW);
+            $campo = htmlspecialchars($campo);
+            return $campo;
+        }
+
+        public function limpiarPassword($campo){
+            $campo = strip_tags($campo);
+            $campo = filter_var($campo, FILTER_UNSAFE_RAW);
+            $campo = htmlspecialchars($campo);
+            return $campo;
+        }
+
+        public function limpiarCorreo($campo){
+            $campo = strip_tags($campo);
+            $campo = filter_var($campo, FILTER_SANITIZE_EMAIL);
+            $campo = htmlspecialchars($campo);
+            return $campo;
+        }
+
+        public function encriptarContrasenia($contrasenia){
+            return password_hash($contrasenia, PASSWORD_DEFAULT);
+        }
+    }
+?>
