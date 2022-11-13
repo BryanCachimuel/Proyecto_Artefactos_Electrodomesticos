c8aa3f1c63c35fee3fb9b1b207beddff3892f1c8
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-12 23:09:44 -0500

    consumiento la función de listar del model hacia el controller

--- controller/productoController.php
+++ controller/productoController.php
@@ -7,9 +7,13 @@
             $this->model = new ProductosModel();
         }
 
-        public function guardarProductos($nombre_producto, $stock, $precio_compra){
-            $id = $this->model->crearProducto($nombre_producto, $stock, $precio_compra);
+        public function guardarProductos($nombre_producto, $descripcion, $stock, $precio_compra){
+            $id = $this->model->crearProducto($nombre_producto, $descripcion, $stock, $precio_compra);
             return ($id != false) ? header("Location:/proyecto_artefactos/views/home/agregarProductos.php?id=".$id) : header("Location:/proyecto_artefactos/views/home/agregarProductos.php");
+        }
+
+        public function verlistaProductos(){
+            return ($this->model->listarProductos()) ? $this->model->listarProductos() : false;
         }
     }
 ?>
