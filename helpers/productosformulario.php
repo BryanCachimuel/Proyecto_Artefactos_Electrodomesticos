bb38094811a987d100102d7aa2f184012a9f6857
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-12 23:10:50 -0500

    controlando el correcto ingreso de información hacia el formulario de registro de artefactos

--- helpers/productosformulario.php
+++ helpers/productosformulario.php
@@ -2,8 +2,9 @@
     require_once("c://xampp/htdocs/proyecto_artefactos/controller/productoController.php");
     $obj = new ProductoController();
     $nombre_producto = $_POST['nombre_producto'];
+    $descripcion = $_POST['descripcion'];
     $stock = $_POST['stock'];
     $precio_compra = $_POST['precio_compra'];
 
-    $obj->guardarProductos($nombre_producto, $stock, $precio_compra);
+    $obj->guardarProductos($nombre_producto, $descripcion, $stock, $precio_compra);
 ?>
