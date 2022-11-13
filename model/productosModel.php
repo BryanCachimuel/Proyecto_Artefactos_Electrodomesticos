51942ed4ac3b3a728e2df55e832989547bd52b88
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-12 23:08:28 -0500

    creando la función para listar todos los artefactos ingresados en la base de datos

--- model/productosModel.php
+++ model/productosModel.php
@@ -8,16 +8,22 @@
             $this->PDO = $pdo->conexion();
         }
 
-        public function crearProducto($nombre_producto, $stock, $precio_compra){
-            $statement = $this->PDO->prepare("INSERT INTO productos values(null,:nombre_producto,:stock,:precio_compra,:valor_total_compra,:precio_venta)");
+        public function crearProducto($nombre_producto, $descripcion, $stock, $precio_compra){
+            $statement = $this->PDO->prepare("INSERT INTO productos values(null,:nombre_producto,:descripcion,:stock,:precio_compra,:valor_total_compra,:precio_venta)");
             $statement->bindParam(":nombre_producto",$nombre_producto);
-            $statement->bindParam(":stock",$stock);
+            $statement->bindParam(":descripcion",$descripcion);
+            $statement->bindParam(":stock",$stock);     
             $statement->bindParam(":precio_compra",$precio_compra);
             $valor_compra = ($stock * $precio_compra);
             $statement->bindParam(":valor_total_compra", $valor_compra); 
             $valor_venta = ((($precio_compra*25)/100)+$precio_compra);
             $statement->bindParam(":precio_venta",  $valor_venta);
             return ($statement->execute()) ? $this->PDO->lastInsertId() : false;
+        }
+
+        public function listarProductos(){
+            $statement = $this->PDO->prepare("SELECT * FROM productos");
+            return ($statement->execute()) ? $statement->fetchAll() : false;
         }
     }
 ?>
