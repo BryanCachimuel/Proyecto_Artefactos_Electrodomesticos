3405b2119155070afaafcd707683c597f8468620
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-16 21:43:20 -0500

    listando en tarjetas la lista de todos los productos registrados

--- views/home/panel_control.php
+++ views/home/panel_control.php
@@ -3,9 +3,30 @@
     if(empty($_SESSION['usuario'])){
         header("Location:iniciar_sesion.php");
     }
+    require_once("c://xampp/htdocs/proyecto_artefactos/controller/productoController.php");
+    $obj = new ProductoController();
+    $productos = $obj->verlistaProductos();
 ?>
 
-    <h1 class="text-center mt-4">Bienvenido <?= $_SESSION['usuario'] ?></h1>
+    <h4 class="text-center mt-4"><i class="fa-sharp fa-solid fa-user-tie"></i> Bienvenido <?= $_SESSION['usuario'] ?></h4>
+    <div class="container mt-3">
+        <div class="row">
+            <?php foreach($productos as $prod) : ?>
+                <div class="col-4">
+                    <div class="card">
+                        <div id="titulo_tarjeta" class="card-header bg-primary text-white">
+                            Producto: <?= $prod[1]?>
+                        </div>
+                        <div class="card-body">
+                            <p class="parrafo"><strong>Descripción:</strong> <?= $prod[2]?></p>
+                            <p class="parrafo"><strong>Stock:</strong> <?= $prod[3]?></p>
+                            <p class="parrafo"><strong>Precio de Venta:</strong> $<?=$prod[6]?></p> 
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
 <?php
     require_once("../head/footer.php")
