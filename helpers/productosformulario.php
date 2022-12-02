84a9110d86aa08a69807e654719b65c70473ff12
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-01 22:55:12 -0500

    controlando que la imagen que se ingrese por el producto se redirija hacia su respectivo repositorio y se guarde en la base de datos

--- helpers/productosformulario.php
+++ helpers/productosformulario.php
@@ -5,6 +5,14 @@
     $descripcion = $_POST['descripcion'];
     $stock = $_POST['stock'];
     $precio_compra = $_POST['precio_compra'];
+    $imagen_producto = $_POST['imagen_producto'];
 
-    $obj->guardarProductos($nombre_producto, $descripcion, $stock, $precio_compra);
+    $fecha=new DateTime();
+    $nombreFoto = $fecha->getTimestamp()."_".$nombre_producto.".png";
+    $tmpFoto = $_FILES["imagen_producto"]["tmp_name"];
+    if($tmpFoto!=""){
+        move_uploaded_file($tmpFoto,"../asset/image/".$nombreFoto);
+    }
+
+    $obj->guardarProductos($nombre_producto, $descripcion, $stock, $precio_compra, $nombreFoto);
 ?>
