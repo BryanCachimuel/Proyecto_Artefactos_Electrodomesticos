928e8004e345233763c20b005f0fa5fc344ab89e
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-03 22:15:14 -0500

    agregando la opción de ver la imagen del producto a editar

--- views/home/editarProducto.php
+++ views/home/editarProducto.php
@@ -14,28 +14,39 @@
             <h3 class="text-white">Editar Producto</h3>
         </div>
         <div class="card-body">
-            <form action="/proyecto_artefactos/helpers/productos/actualizarProductos.php" method="POST" autocomplete="off">
+            <form action="/proyecto_artefactos/helpers/productos/actualizarProductos.php" method="POST" autocomplete="off" enctype="multipart/form-data">
                 <div>
                     <input type="hidden" name="id" class="form-control" id="id" value="<?= $producto['id'] ?>">
                 </div>
                 <div class="mb-3">
-                    <label for="nombre_producto" class="form-label">Contenido:</label>
+                    <label for="nombre_producto" class="form-label">Nombre Producto:</label>
                     <input type="text" name="nombre_producto" class="form-control" id="nombre_producto" value="<?= $producto['nombre_producto'] ?>">
                 </div>
                 <div class="mb-3">
                     <label for="descripcion" class="form-label">Descripción:</label>
                     <textarea class="form-control" name="descripcion"  id="descripcion" rows="2"><?= $producto['descripcion'] ?></textarea>
                 </div>
-                <div class="mb-3">
-                    <label for="stock" class="form-label">Dirección: </label>
-                    <input type="text" name="stock" class="form-control" id="stock" value="<?= $producto['stock'] ?>">
+                <div class="form-group row">
+                  <label for="stock" class="col-sm-2 col-form-label">Stock:</label>
+                  <div class="col-sm-4">
+                    <input type="number" name="stock" class="form-control" id="stock" value="<?= $producto['stock'] ?>">
+                  </div>
+                  <label for="precio_compra" class="col-sm-2 col-form-label">Precio: </label>
+                  <div class="col-sm-4">
+                    <input type="number" name="precio_compra" class="form-control" id="precio_compra"  value="<?= $producto['precio_compra'] ?>">
+                  </div>
                 </div>
-                <div class="mb-3">
-                    <label for="precio_compra" class="form-label">Contacto 1:</label>
-                    <input type="text" name="precio_compra" class="form-control" id="precio_compra"  value="<?= $producto['precio_compra'] ?>">
+                <div class="mb-3 mt-3">
+                  <label for="imagen_producto" class="form-label">Imagen Producto:</label>
+                    <div class="mt-2 mb-2">
+                        <img class="img-thumbnail" width="160px" src="/proyecto_artefactos/asset/image/<?php echo $producto['imagen_producto'];?>" />
+                    </div>
+                  <input type="file" class="form-control" accept="image/*" name="imagen_producto" id="imagen_producto" require="" value="<?= $producto['imagen_producto'] ?>">
                 </div>
-                <button type="submit" class="btn btn-primary">Actualizar Producto</button>
-                <a href="agregarProductos.php" class="btn btn-warning">Cancelar</a>
+                <div class="mb-3 mt-4">
+                    <button type="submit" class="btn btn-primary">Actualizar Producto</button>
+                    <a href="agregarProductos.php" class="btn btn-warning">Cancelar</a>
+                </div>
             </form>
         </div>
     </div>  
