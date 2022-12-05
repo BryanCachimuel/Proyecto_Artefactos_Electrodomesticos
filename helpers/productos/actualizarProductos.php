85f4e71dbf192ce63c6dd7155046c5f7c215e1d7
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-04 22:24:53 -0500

    controlando el envió de la imagen del producto desde el formulario de registro de producto y rediriengiendo la imagen hacia su directorio seleccionado

--- helpers/productos/actualizarProductos.php
+++ helpers/productos/actualizarProductos.php
@@ -6,6 +6,14 @@
     $descripcion = $_POST['descripcion'];
     $stock = $_POST['stock'];
     $precio_compra = $_POST['precio_compra'];
+    $imagen_producto = $_POST['imagen_producto'];
 
-    $obj->actualizarRegistroProducto($id, $nombre_producto, $descripcion, $stock, $precio_compra);
+    $fecha=new DateTime();
+    $nombreFoto = $fecha->getTimestamp()."_".$nombre_producto.".png";
+    $tmpFoto = $_FILES["imagen_producto"]["tmp_name"];
+    if($tmpFoto!=""){
+        move_uploaded_file($tmpFoto,"/proyecto_artefactos/asset/image/".$nombreFoto);
+    }
+
+    $obj->actualizarRegistroProducto($id, $nombre_producto, $descripcion, $stock, $precio_compra, $nombreFoto);
 ?>
