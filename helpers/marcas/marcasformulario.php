f68d5cd5ff7224af9a8b04fa22485b0cee426e02
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-07 22:29:56 -0500

    controlando la información que se ingrese por el formulario de registro de marcas

--- helpers/marcas/marcasformulario.php
+++ helpers/marcas/marcasformulario.php
@@ -2,6 +2,9 @@
     require_once("c://xampp/htdocs/proyecto_artefactos/controller/marcasController.php");
     $obj = new MarcasController();
     $marcas = $_POST['marcas'];
+    $pais = $_POST['pais'];
+    $nombre_proveedor = $_POST['nombre_proveedor'];
+    $contacto_proveedor = $_POST['contacto_proveedor'];
 
-    $obj->guardarMarca($marcas);
+    $obj->guardarMarca($marcas, $pais, $nombre_proveedor, $contacto_proveedor);
 ?>
