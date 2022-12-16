c026337031f3c17ce00ada4649b02cae3fae91a5
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-15 22:32:40 -0500

    estructurando adecuadamente los atributos que van a ser editados mediante el formulario de actualización de productos

--- helpers/productos/actualizarProductos.php
+++ helpers/productos/actualizarProductos.php
@@ -6,14 +6,10 @@
     $descripcion = $_POST['descripcion'];
     $stock = $_POST['stock'];
     $precio_compra = $_POST['precio_compra'];
-    $imagen_producto = $_POST['imagen_producto'];
 
     $fecha=new DateTime();
     $nombreFoto = $fecha->getTimestamp()."_".$nombre_producto.".png";
     $tmpFoto = $_FILES["imagen_producto"]["tmp_name"];
-    if($tmpFoto!=""){
-        move_uploaded_file($tmpFoto,"/proyecto_artefactos/asset/image/".$nombreFoto);
-    }
 
-    $obj->actualizarRegistroProducto($id, $nombre_producto, $descripcion, $stock, $precio_compra, $nombreFoto);
+    $obj->actualizarRegistroProducto($id, $nombre_producto, $descripcion, $stock, $precio_compra);
 ?>
