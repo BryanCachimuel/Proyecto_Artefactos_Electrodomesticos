d15ee38716e39efdb7a1d6ab69c1d91e4f6a93d6
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-01 22:18:37 -0500

    añadiendo las rutas para los archivos con estilos

--- views/head/head.php
+++ views/head/head.php
@@ -8,6 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- enlazar todos los archivos externos -->
     <link rel="stylesheet" href="/proyecto_artefactos/asset/css/estilos.css">
+    <link rel="stylesheet" href="/proyecto_artefactos/asset/css/normalize.css">
+    <link rel="stylesheet" href="/proyecto_artefactos/asset/css/cuerpo.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css">
 </head>
