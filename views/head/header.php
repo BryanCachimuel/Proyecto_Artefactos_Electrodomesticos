1a6eac24907ab0d3964966a72d25037a60ab50f4
BryanCachimuel <bryanloyo56@gmail.com>
2023-01-01 22:19:43 -0500

    añadiendo la vista para el panel de control son inicio de sesión

--- views/head/header.php
+++ views/head/header.php
@@ -28,7 +28,6 @@
                             <a class="nav-link" href="#">Contactanos</a>
                         </li>
                     </ul>
-                    <a href="/proyecto_artefactos/views/home/iniciar_sesion.php" class="boton">Iniciar Sesión</a>
                     <a href="/proyecto_artefactos/views/home/registrar.php" class="boton">Registrate</a>
                 </div>
             <?php else: ?>
@@ -73,10 +72,88 @@
             <?php endif ?>           
         </div>
     </nav>
- </div>
+  </div>
 </div>
 
 <!-- esta clase solo se va autilizar para agregar el código ya sea externo o el código que se va a estar visualizando en cada pestaña -->
 <div class="fondo">
+
+<?php if(empty($_SESSION['usuario'])) : ?>
+    <header class="hero">
+        <nav class="nav container"></nav>
+
+        <section class="hero__container container">
+            <h1 class="hero__title">Almacen de Electrodomesticos</h1>
+            <p class="hero__paragraph">El almacen a su disposición con todos los Electrodomesticos que ustedes desee cotizar y 
+                a los mejores precios del mercado
+            </p>
+               
+            <a href="/proyecto_artefactos/views/home/iniciar_sesion.php" class="cta">Iniciar Sesión</a>
+        </section>
+    </header>
+
+    <main>
+        <section class="container about">
+            <h2 class="subtitle">¿Qué aprenderás en encontraras en nuestro almacen?</h2>
+            <p class="about__paragraph">Todo lo necesario para que tu lo puedas comprar y llenar tu casa con los Electrodomesticos
+                que siempre as soñado
+            </p>
+
+            <div class="about__main">
+                <article class="about__icons">
+                    <img src="./images/shapes.svg" class="about__icon">
+                    <h3 class="about__title">CSS Layouts</h3>
+                    <p class="about__paragrah">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae tempore
+                        porro eius facilis?</p>
+                </article>
+
+                <article class="about__icons">
+                    <img src="./images/paint.svg" class="about__icon">
+                    <h3 class="about__title">Animaciones</h3>
+                    <p class="about__paragrah">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae tempore
+                        porro eius facilis?</p>
+                </article>
+
+                <article class="about__icons">
+                    <img src="./images/code.svg" class="about__icon">
+                    <h3 class="about__title">Sass</h3>
+                    <p class="about__paragrah">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae tempore
+                        porro eius facilis?</p>
+                </article>
+            </div>
+        </section>
+
+    <footer class="footer">
+        <section class="footer__container container">
+            <nav class="nav nav--footer">
+                <h2 class="footer__title">Almacen de Electrodomesticos.</h2>
+            </nav>
+
+            <form class="footer__form" action="https://formspree.io/f/mknkkrkj" method="POST">
+                <h2 class="footer__newsletter">Suscribete a la newsletter</h2>
+                <div class="footer__inputs">
+                    <input type="email" placeholder="Email:" class="footer__input" name="_replyto">
+                    <input type="submit" value="Registrate" class="footer__submit">
+                </div>
+            </form>
+        </section>
+
+        <section class="footer__copy container">
+            <div class="footer__social">
+                <a href="#" class="footer__icons"><img src="./images/facebook.svg" class="footer__img"></a>
+                <a href="#" class="footer__icons"><img src="./images/twitter.svg" class="footer__img"></a>
+                <a href="#" class="footer__icons"><img src="./images/youtube.svg" class="footer__img"></a>
+            </div>
+
+            <h3 class="footer__copyright">Derechos reservados &copy; Jordan Alexander</h3>
+        </section>
+    </footer>
+
+    <script src="./js/slider.js"></script>
+    <script src="./js/questions.js"></script>
+    <script src="./js/menu.js"></script>
+<?php else: ?>
+
+<?php endif ?>
 
 
