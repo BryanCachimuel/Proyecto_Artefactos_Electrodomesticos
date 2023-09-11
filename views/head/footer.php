eff5484ebffeb5ae37f43b82172812894c56b6cd
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-10 20:59:17 -0500

    añadiendo la funcionalidad de DataTable para la tabla de Usuarios

--- views/head/footer.php
+++ views/head/footer.php
@@ -35,6 +35,33 @@
           ],
       });
     });
+
+    $(document).ready( function () {
+        $('#usuarios').DataTable({
+            "language":{
+              "decimal": "",
+              "emptyTable": "No hay información",
+              "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+              "infoEmpty": "Mostrando 0 a 0 de 0 Entradas",
+              "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+              "infoPostFix": "",
+              "thousands": ",",
+              "lengthMenu": "Mostrar _MENU_ Entradas",
+              "loadingRecords": "Cargando...",
+              "processing": "Procesando...",
+              "search": "Buscar:",
+              "zeroRecords": "Sin resultados encontrados",
+              "paginate": {
+                  "first": "Primero",
+                  "last": "Ultimo",
+                  "next": "Siguiente",
+                  "previous": "Anterior"
+              }
+          },
+          "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, "All"]
+          ],
+      });
+    });
 </script>
 
 
